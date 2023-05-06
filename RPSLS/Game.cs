b76cf57e4bc185dbcfcdfe88d51844f17cd77748
fatimaz101 <fatimayasin100@gmail.com>
2023-05-06 11:45:18 -0500

    Added Welcome message

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,19 @@ namespace RPSLS
         //Member Methods (CAN DO)
         public void WelcomeMessage()
         {
-            Console.WriteLine("Welcome to RPSLS! Here are the rules:\n");
+            Console.WriteLine("Welcome to RPSLS! Here are the game rules:" +
+                "Rock crushes Scissors" +
+                "Scissors cuts Paper" +
+                "Paper covers Rock " +
+                "Rock crushes Lizard" +
+                "Lizard poisons Spock" +
+                "Spock smashes Scissors" +
+                "Scissors decapitates Lizard" +
+                "Lizard eats Paper" +
+                "Paper disproves Spock" +
+                "Spock vaporizes Rock");
+
+            
         }
 
         public int ChooseNumberOfHumanPlayers()
@@ -46,6 +59,14 @@ namespace RPSLS
 
         public void RunGame()
         {
+          //Step 1. Display the rules of the game
+          //Step 2. Ask how many human players will be playing
+          //Step 3. Get the names from the players
+          //Step 4. Let each player choose a gesture
+          //Step 5. Compare the gestures of each player and give one a point
+          //Step 6. Loop step 4 and 5 until a player reaches 2 points
+          //Step 7. Display winner 
+          
             WelcomeMessage();
         }
     }
